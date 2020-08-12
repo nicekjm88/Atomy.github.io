b73f0b1f6e7b47101ee767b2412e33321311bb51
jmkim88 <nicekjm88@gmail.com>
2020-08-12 11:36:33 +0900

    ajax test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("data-include");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("data-include");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
 $(function () {
 
   // Ajax Include
@@ -61,7 +89,7 @@ $(function () {
   })(window, window.jQuery);
 
   // 아코디언 매뉴
-  $('#product').accordion({
+  $('.product-details').accordion({
     collapsible: true,
     titleSelector: '.product-title',
     contSelector: '.product-content',
@@ -111,4 +139,11 @@ $(function () {
       $('html, body').animate( {scrollTop: offset_top}, 500 );
   });   
 
+  includeHTML();
+});
+
+$(window).load(function() {
+  $('.nav').on('mouseenter', function() {
+    console.log('클릭');
+  });
 });
