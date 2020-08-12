849d599344cc74f46ae6182bb038006a561ddd2c
jmkim88 <nicekjm88@gmail.com>
2020-08-12 13:05:41 +0900

    ㄱㄱㄱ

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,17 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("data-include");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("data-include");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-};
-
 $(function () {
 
-  // Ajax Include
-  // (function() {
-  //   fetch("_include/nav.html")
-  //   .then(function(response) {
-  //     return response.text()
-  //   })
-  //   .then(function(data) {
-  //     document.querySelector("#side-nav").innerHTML = data;
-  //   });
-  // })();
+  // 인클루드
+  (function(global, $) {
+    'use strict';
+
+    var includes = $('[data-include]');
+
+    $.each(includes, function (){
+      var file ='_include/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+
+  })(window, window.jQuery);
 
   // 스크롤 애니메이션
   (function scrollInteraction(global, $) {
@@ -139,11 +113,11 @@ $(function () {
       $('html, body').animate( {scrollTop: offset_top}, 500 );
   });   
 
-  includeHTML();
 });
 
 $(window).load(function() {
   $('.nav').on('mouseenter', function() {
-    console.log('클릭');
+    console.log('이벤트');
   });
 });
+
